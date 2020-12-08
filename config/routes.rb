71bdda4d690506966851766c105cf0580dc66323
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-08 16:05:19 -0400

    Devise views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :events # , :only => [:new, :create, :index]
-  devise_for :users, path: '',path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, path: '', path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  routes "events#index"
 end
